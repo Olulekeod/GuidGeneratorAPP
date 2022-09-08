35fb315e49d481289b247c5f635db92a70a1cf6b
OlulekeO <Kinzmah@DESKTOP-D29O9PH>
2022-09-08 03:58:49 -0700

    Add project files.

---
+++ GuidGeneratorAPP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+
+            for (int i = 0; i < 500; i++)
+            {
+                Guid obj = Guid.NewGuid();
+                Console.WriteLine(obj.ToString());
+                //Console.ReadLine();
+                //return -1;
+                //Console.WriteLine(i);
+            }
+
+
+
+        }
+    }
+}
